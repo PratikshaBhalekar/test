a27ab70393c6e4dd2e485d199d80a58fc1749497
PratikshaBhalekar <bhalekars431@gmail.com>
2024-01-23 10:39:30 +0530

    Revert "Vadapav"
    This reverts commit c8a2146fdadb5fddf57aeb9c19adf4e721d957c4.

--- hungry.py
+++ hungry.py
@@ -2,7 +2,7 @@ hungry= input("are you hungry ")
 if hungry=="yes":
     print("eat samosa ")
     print("eat pizza")
-    print("eat vadapav")
+    print("eat burger")
 
 else:
     print("Do your hw")
